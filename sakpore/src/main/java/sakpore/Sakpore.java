0d8107eee3e63e1200035ca43ec0c323a381ee45
intannurnaisyilla <intansyilla10@gmail.com>
2025-09-17 12:03:21 +0700

    kasirSakpore

---
+++ sakpore/src/main/java/sakpore/Sakpore.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sakpore;
+
+/**
+ *
+ * @author Acer
+ */
+public class Sakpore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
